6c297b9523107f8fd4a6392938ea4bc2ed7dccf0
transeunteprompts-pixel <transeunteprompts@gmail.com>
2026-03-18 23:22:30 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-import withPWA from "next-pwa";
-
 const nextConfig = {
-  reactStrictMode: true,
+  turbopack: {},
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+export default nextConfig;
